d5c30ddb7881d11b43347bb59df495b62f5f3170
Cícero Sousa <cicerosousa.le@gmail.com>
2023-05-29 21:39:08 -0300

    primeiro commit JS

---
+++ Aula3/index.js
@@ -0,0 +1,6 @@
+
+
+console.log('cicero jorge');    //String
+console.log("'cicero jorge'");  //String  Esse exercício mostra como utilizar as (Aspas)
+console.log(`"cicero" 'jorge'`); //String 
+console.log(35,05,96, 'cicero jorge leandro de sousa')
