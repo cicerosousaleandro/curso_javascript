3593b075ca62a025e372dd0523da8f877e5badeb
Cícero Sousa <cicerosousa.le@gmail.com>
2023-05-31 21:52:57 -0300

    aula 4

---
+++ Aula4/comentarios.js
@@ -0,0 +1,17 @@
+// aqui logo depois das barras eu posso fazer comentários para melhor organizar o código
+console.log("Eu sou o Cícero");// Eu posso comentar na frente do código também!
+// Se eu comentar aqui, nada vai acontecer também e o código vai funcionar. 
+console.log("Linha 1");
+//comentário
+console.log("Linha 2");
+//Mais um comentário
+console.log("Linha 3");
+//Outro comentário
+//mais uma linha
+console.log("Linha 4");
+
+
+/* Essa aqui é uma forma de 
+comentar em mais de uma inha*/
+
+// Essa aqui é uma forma de comentar e uma única linha
